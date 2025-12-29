2e00aab3b840248534619789708cbf4f196cf09f
leevccc <94272011+leevccc@users.noreply.github.com>
2025-12-30 00:32:18 +0800

    Fix: WzImage导出Xml

--- src/main/java/orange/wz/gui/component/menu/WzImageMenu.java
+++ src/main/java/orange/wz/gui/component/menu/WzImageMenu.java
@@ -280,7 +280,7 @@ public final class WzImageMenu extends JPopupMenu {
                     MainFrame.getInstance().setStatusText("文件 %s 解析失败: %s", wzImage.getName(), wzImage.getStatus().getMessage());
                     throw new RuntimeException();
                 }
-                wzImage.exportToXml(Path.of(data.getExportPath()), data.getIndent(), data.getMeType());
+                wzImage.exportToXml(Path.of(data.getExportPath(), wzImage.getName() + ".xml"), data.getIndent(), data.getMeType());
             }
         });
     }
